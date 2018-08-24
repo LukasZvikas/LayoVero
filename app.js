3a0877fc289f63d2b7da444f9807561757a38f4c
LukasZvikas <lzvikas1@gmail.com>
2018-08-24 14:25:26 -0700

    added heroku configuration in root backend file

--- app.js
+++ app.js
@@ -36,4 +36,14 @@ require("./routes/routeRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(PORT);

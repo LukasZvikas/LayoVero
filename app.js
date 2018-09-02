0bb322d9fa1313301db403c1671bd39394937f39
LukasZvikas <lzvikas1@gmail.com>
2018-09-02 10:48:04 -0400

    added console log for a main server file

--- app.js
+++ app.js
@@ -44,10 +44,10 @@ if (process.env.NODE_ENV !== "production") {
   // app.use(webpackMiddleware(webpack(webpackConfig)));
 } else {
   const path = require("path");
-  app.use(express.static('client/build'));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "index.html"));
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/index.html"));
   });
 }
 
-app.listen(PORT);
+app.listen(PORT, () => console.log("Listening"));

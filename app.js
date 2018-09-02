6d4ae3f39f42f41ca55064facf8e5629408649d3
LukasZvikas <lzvikas1@gmail.com>
2018-09-02 00:33:13 -0400

    changed static express line

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV !== "production") {
   // app.use(webpackMiddleware(webpack(webpackConfig)));
 } else {
   const path = require("path");
-  app.use(express.static(__dirname + 'client/build'));
+  app.use(express.static('client/build'));
   app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "index.html"));
   });

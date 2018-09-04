190d3466ef4eb10510579da70a8f5b22375716f3
LukasZvikas <lzvikas1@gmail.com>
2018-09-04 19:41:11 -0400

    changed app.js get config

--- app.js
+++ app.js
@@ -38,9 +38,9 @@ const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV == "production") {
   const path = require("path");
-  app.use(express.static('client/build'));
+  app.use(express.static('build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+    res.sendFile(path.join(__dirname, 'build/index.html'));
   });
 
   // app.use(express.static("client/build"));

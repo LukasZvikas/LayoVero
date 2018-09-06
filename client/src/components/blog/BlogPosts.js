0d77bddbcd2ac3c42739ef3aaa02d4ace59b7d36
LukasZvikas <lzvikas1@gmail.com>
2018-09-06 12:28:23 -0400

    trying different import pattern for imagesw

--- client/src/components/blog/BlogPosts.js
+++ client/src/components/blog/BlogPosts.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import ProgImageLoading from "./loadingImage";
 
 import img from "../../../../uploads/4b8b378225e5dd1c79503fbfd30156e5.jpg";
-import imgThumb from "../../../../uploads/4b8b378225e5dd1c79503fbfd30156e5thumb.jpg";
+import imgThumb from "../../../../uploads/4b8b378225e5dd1c79503fbfd30156e5.jpg";
 
 class BlogPosts extends Component {
   componentDidMount() {
@@ -21,8 +21,8 @@ class BlogPosts extends Component {
           className="blog-posts-main-post"
         >
           <ProgImageLoading
-            regPhoto={img}
-            thumbPhoto={imgThumb}
+            regPhoto={"../../../.." + post.image + ".jpg"}
+            thumbPhoto={"../../../.." + post.image + "thumb.jpg"}
             imageClass={"main"}
           />
           {/*<img className={`blog-posts__image--${2}`} src={"../../images/logoyoutube.png"} />
